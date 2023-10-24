7753c0236d49192bd2701fb12cbaec4f0413b2d4
1Max58 <>
2023-10-24 22:12:04 +0300

    User input and simple calculations implemented

--- src/com/example/simplecalculator/SimpleCalculator.java
+++ src/com/example/simplecalculator/SimpleCalculator.java
@@ -1,7 +1,31 @@
 package com.example.simplecalculator;
 
+import java.util.Scanner;
+
 public class SimpleCalculator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Please enter two numbers to calculate sum, difference, product and a quotient.");
+
+        System.out.print("First number: ");
+        float firstNumber = input.nextFloat();
+
+        System.out.print("Second number: ");
+        float secondNumber = input.nextFloat();
+        input.close();
+
+        float sum = firstNumber + secondNumber;
+        System.out.printf("%.2f + %.2f = %.2f%n", firstNumber, secondNumber, sum);
+
+        float difference = firstNumber - secondNumber;
+        System.out.printf("%.2f - %.2f = %.2f%n", firstNumber, secondNumber, difference);
+
+        float product = firstNumber * secondNumber;
+        System.out.printf("%.2f * %.2f = %.2f%n", firstNumber, secondNumber, product);
+
+        float quotient;
+        quotient = firstNumber / secondNumber;
+        System.out.printf("%.2f / %.2f = %.2f%n", firstNumber, secondNumber, quotient);
     }
 }
